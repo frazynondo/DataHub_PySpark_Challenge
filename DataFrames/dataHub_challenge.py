aa2f199566658395f6f690abf3554425a897b9f5
frazynondo <Eugene@905>
2021-06-14 01:18:13 -0700

    Simple Pyspark Challenge

--- DataFrames/dataHub_challenge.py
+++ DataFrames/dataHub_challenge.py
@@ -1,21 +1,29 @@
-import sys
-from datetime import datetime
-from logging import Logger
+try:
+    import io
+    from datetime import datetime
+    from logging import Logger
 
-from pyspark.sql import *
-from pyspark.sql import functions as f
-from pyspark.sql.functions import col, lit, when, to_date, udf, rank
-from pyspark.sql.types import StructType, StringType, IntegerType, StructField, DateType, FloatType, LongType, \
-    DoubleType, DecimalType
-from pyspark.sql.functions import sum, avg, max, min, mean, count
+    from pyspark.sql import *
+    from pyspark.sql import functions as f
+    from pyspark.sql.functions import col, lit, when, to_date, udf, rank
+    from pyspark.sql.types import StructType, StringType, IntegerType, StructField, DateType, FloatType, LongType, \
+        DoubleType, DecimalType
+
+    from google.cloud import storage
+    import pandas as pd
+    from pyspark.sql.functions import sum, avg, max, min, mean, count
+except ImportError as e:
+    print("Something wrong with the import {}".format(e))
 
 
 # This is used to get trans_fact files ready but with a little modification it can be generic to accommodate many
 # different stores with different names that doesnt qualify for a wild card path call
 
 class Job_func(object):
-    # def __init__(self):
-    #     pass
+    def __init__(self):
+        pass
+        # self.QueResult = []
+
     @staticmethod
     def Schemas(B):
         trans_keys = ""
@@ -30,30 +38,37 @@ class Job_func(object):
             StructField("trans_dt", StringType()),
             StructField("sales", DoubleType()),
             StructField("units", IntegerType()),
-            # StructField("trans_key", StringType())
-            StructField(trans_keys, DecimalType(35, 0))
+            StructField("trans_key", StringType())
+            # StructField(trans_keys, DecimalType(35, 0))
         ])
         return Schema
 
     # To combine trans_fact data
     @staticmethod
-    def combineData(sparks):
-        result1 = sparks.read.csv(path="Data/trans_fact_1.csv", sep=",",
-                                  header=True, schema=Job_func.Schemas(0), mode="FAILFAST")
-        result1 = Job_func.combine_DF(spark, result1, 2, 11)
+    def combineData(sparks, QueRes):
+        # P = ExtractFromGCS()
+        # QueResult = P.get_GCS_Data()
+        # Converts the string to Pandas DF
+        pads = pd.read_csv(io.BytesIO(QueRes[0]), encoding="utf-8", sep=",")
+        # print(pads.head())
+        # Converts Pandas DF to Spark DataFrame - There should be a
+        # straight way to do this without using Pandas DF
+        result1 = spark.createDataFrame(pads, schema=Job_func.Schemas(0))
+        result1 = Job_func.combine_DF(spark, result1, 4, 10, QueRes)
         return result1
 
     @staticmethod
-    def combine_DF(sparks, result, left, right):
+    def combine_DF(sparks, result, left, right, QueRes):
         for i in range(left, right):
-            if i <= 5:
-                if i == 4 or i == 5:
-                    temp = Job_func.changeSchema(sparks, i)
+            # Most files under 4th index are "Trans_key" Column named and above are "Trans_id" column named
+            if i <= 4:
+                if i == 3 or i == 4:
+                    temp = Job_func.changeSchema(sparks, i, QueRes)
                 else:
-                    temp = sparks.read.csv(path="Data/trans_fact_{}.csv".format(i), sep=",",
-                                           header=True, schema=Job_func.Schemas(0))
+                    pads = pd.read_csv(io.BytesIO(QueRes[i]), encoding="utf-8", sep=",")
+                    temp = spark.createDataFrame(pads, schema=Job_func.Schemas(0))
             else:
-                temp = Job_func.changeSchema(sparks, i)
+                temp = Job_func.changeSchema(sparks, i, QueRes)
             if i == 4 or i == 6 or i == 8:
                 uds = udf(lambda x: datetime.strptime(x, '%m/%d/%Y'), DateType())
                 temp = temp.withColumn("trans_dt", uds(col("trans_dt")))
@@ -62,20 +77,18 @@ class Job_func(object):
         return result
 
     @staticmethod
-    def changeSchema(sparks, index):
-        try:
-            data = sparks.read.csv(path="Data/trans_fact_{}.csv".format(index), sep=",",
-                                   header=True, inferSchema=True)
-            if index > 5:
-                data = data.withColumnRenamed("trans_id", "trans_key")
-            data = data.withColumn("store_location_key",
-                                   data["store_location_key"]
-                                   .cast(IntegerType())).withColumn("collector_key", data["collector_key"]
-                                                                    .cast(LongType())) \
-                .withColumn("trans_key", data["trans_key"].cast(DecimalType(35, 0))) \
-                .withColumn("product_key", data["product_key"].cast(LongType()))
-        except IOError as e:
-            print("Something is wrong in the change Schema method --> {}".format(e))
+    def changeSchema(sparks, index, QueR):
+        # data = sparks.read.csv(path="Data/trans_fact_{}.csv".format(index), sep=",",
+        #                        header=True, inferSchema=True)
+        data = ExtractFromGCS.String_To_Pd_to_dataFrame(index, QueR)
+        if index > 3:
+            data = data.withColumnRenamed("trans_id", "trans_key")
+        data = data.withColumn("store_location_key",
+                               data["store_location_key"]
+                               .cast(IntegerType())).withColumn("collector_key", data["collector_key"]
+                                                                .cast(LongType())) \
+            .withColumn("trans_key", data["trans_key"].cast(DecimalType(35, 0))) \
+            .withColumn("product_key", data["product_key"].cast(LongType()))
         return data
 
     # Given that we only have one column with nulls we can write this for the purpose of simplicity
@@ -92,15 +105,83 @@ class Job_func(object):
         res = data1.join(data2, data1[key1] == data2[key2])
         return res
 
-    @staticmethod
-    def Single_CSV_reader(file, sparks):
+
+"""
+************** NEW CLASS SPACE ****************
+"""
+
+"""
+Extract the data from GCS Bucket 
+"""
+
+
+class ExtractFromGCS(object):
+    def __init__(self):
+        self.storage_client = storage.Client.from_service_account_json("Key/GCP_Service.json")
+        self.BUCKET_NAME = "my-gcs-bucket"
+        self.bucket = self.storage_client.get_bucket(self.BUCKET_NAME)
+        self.data = list(self.bucket.list_blobs(prefix=""))
+
+    # Save the string names of trans_fact items from the bucket in a que
+    def GCS_Data(self):
         try:
-            res = sparks.read.csv(path=file, sep=",",
-                                  header=True, inferSchema=True)
-        except OSError:
-            print("Could not open/read file: {}".format(file))
-            sys.exit()
-        return res
+            que = []
+            for file in self.data:
+                TP = file.name
+                if TP[0].lower() == "t":
+                    print(TP, "FILE NAME")
+                    que.append(TP)
+        except IOError as er:
+            print("Problems with reading bucket content name".format(er))
+        return que
+
+    # Extract location.csv and product.csv name into a que
+    def GCS_Data_Non_Trans_fact(self):
+        Qu = []
+        try:
+            for file in self.data:
+                # TP = file.name
+                if file.name[0].lower() == "l" or file.name[0].lower() == "p":
+                    print(file.name, "FILE NAME")
+                    Qu.append(file.name)
+        except IOError as Ex:
+            print("Issues with reading a file: {}".format(Ex))
+        return Qu
+
+    # Handle one vs more items in the bucket
+    def convertData(self, ques):
+        # ques = self.GCS_Data()
+        rez = []
+        if len(ques) == 0:
+            print("No Item in the bucket")
+            exit()
+        if len(ques) == 1:
+            blop = self.bucket.blob(ques.pop(0))
+            rez.append(blop.download_as_string())
+            return rez
+        else:
+            while ques:
+                T = ques.pop(0)
+                blop = self.bucket.blob(T)
+                rez.append(blop.download_as_string())
+        return rez
+
+    def get_GCS_Data(self, Q):
+        return self.convertData(Q)
+
+    @staticmethod
+    def String_To_Pd_to_dataFrame(index, QueR):
+        # P = ExtractFromGCS()
+        # QueR = P.get_GCS_Data()
+        pads = pd.read_csv(io.BytesIO(QueR[index]), encoding="utf-8", sep=",")
+        print(pads.head())
+        reads = spark.createDataFrame(pads)
+        return reads
+
+
+"""
+************** NEW CLASS SPACE ****************
+"""
 
 
 class Results(object):
@@ -113,48 +194,64 @@ class Results(object):
             print("Something is wrong with the passed in parameters: {}".format(e))
 
 
+"""
+************** NEW CLASS SPACE ****************
+"""
+
 if __name__ == "__main__":
     spark = SparkSession.builder \
         .appName("SparkSQL") \
         .master("local[3]") \
         .getOrCreate()
 
-    # Combine Trans_fact data and Change -1s to 0s values
-    df = Job_func.combineData(spark)
+    P = ExtractFromGCS()
+    Q1 = P.GCS_Data()
+    QueResult = P.get_GCS_Data(Q1)
+    # Combine Trans_fact data and Change -1 to 0 values
+    df = Job_func.combineData(spark, QueResult)
     df = Job_func.nullsToZero(df, "collector_key", "COLLECT_KEY")
     df.printSchema()
-
-    # df = df.withColumn("trans_dt", f.to_timestamp(f.col("trans_dt"), "yyyy-dd-mm"))
-    # df = df.orderBy("trans_dt")
-    # df.show(20)
-    # print("Counted total is {}".format(df.count()))
+    df.show(10)
 
     # Location CSV Data
-    file = "Data/location.csv"
-    Loc = Job_func.Single_CSV_reader(file, spark)
+    # file = QueResult[10]
+    Q2 = P.GCS_Data_Non_Trans_fact()
+    QueResult = P.get_GCS_Data(Q2)
+    Loc = ExtractFromGCS.String_To_Pd_to_dataFrame(0, QueResult)
+    # Loc = Job_func.Single_CSV_reader(file, spark)
+    print("LOCATION RESULTS")
     Loc = Loc.drop("postal_code").drop("banner").drop("region").drop("store_location_key")
-    Loc.show()
+    Loc.show(10)
 
-    # Product CSV Data
-    file = "Data/product.csv"
-    Prod = Job_func.Single_CSV_reader(file, spark)
-    Prod = Prod.drop("item_description").drop("upc").drop("item_description").drop("sku")
+    #
+    # # Product CSV
+    print("PRODUCT RESULTS")
+    Prod = ExtractFromGCS.String_To_Pd_to_dataFrame(1, QueResult)
+    Prod = Prod.drop("item_description")
+    Prod.printSchema()
+    Prod.show(10)
 
-    # Join Location with Combined Trans_fact CSV Data
+    # # Join Location with Combined Trans_fact CSV Data
     Temps = Job_func.joinDataSets(df, Loc, "store_location_key", "store_num")
+    Temps = Temps.withColumn("sales", Temps["sales"].cast(DoubleType())) \
+        .withColumn("COLLECT_KEY", Temps["COLLECT_KEY"].cast(LongType()))
+
+    # replace all null values with zero in sales
+    Temps = Temps.na.fill(value=0, subset=["sales"])
     Temps = Temps.orderBy("trans_dt")
     Temps = Temps.drop("city")
-    Temps.show()
+    Temps.show(10)
+    Temps.printSchema()
 
     # Total Sales per province
-    province_sales = Temps.groupBy("province").sum("sales").show()
+    province_sales = Temps.groupBy("province").sum("sales").show(10)
 
     # Province and it stores' Total
-    both_sales = Temps.groupBy("province", "store_num").sum("sales")
-    print("PROVINCE AND STORE TOTAL")
-    both_sales.orderBy("province").show()
+    # both_sales = Temps.groupBy("province", "store_num").sum("sales")
+    # print("PROVINCE AND STORE TOTAL")
+    # both_sales.orderBy("province").show(10)
 
-    # Top store vs average store of the Province
+    # # Top store vs average store of the Province
     Performance = Temps.groupBy("province").agg(
         sum("sales").alias("Total_sales"),
         max("sales").alias("Province_Top_Store_sales"),
@@ -238,3 +335,4 @@ if __name__ == "__main__":
     # top_ten_pro.select("*", rank().over(window).alias("Rank")).filter(col("Rank") <= 10).show()
     top_ten_pro = Results.ranking(top_ten_pro, top_ten_pro["department"], top_ten_pro["sales"], 10)
     top_ten_pro.show()
+
